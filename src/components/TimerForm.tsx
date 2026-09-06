57ac00f56cf616b9912edaec153b908e50935499
i994114 <i994114@hotmail.com>
2026-09-06 17:41:31 +0900

    いったん、App.tsxでtimerカードが表示されるところまで確認できるコードに修正

--- src/components/TimerForm.tsx
+++ src/components/TimerForm.tsx
@@ -1,9 +1,20 @@
 import {useState} from "react";
 
+
+
+
 function TimerForm() {
 
+
 	return (
-		<h1>TimerForm</h1>
+		<form className="" >
+			<input 
+				type="text"
+				value="test"
+				placeholder="タイマー名を入力"
+			/>
+			<button className="" type="submit" >追加</button>
+		</form>
 	);
 }
 
