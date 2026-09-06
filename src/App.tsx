5d1117bd3e29be089f15e46016d79d0cf61868d6
i994114 <i994114@hotmail.com>
2026-09-06 17:44:42 +0900

    一覧表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import TimerForm from './components/TimerForm';
-import type { Timer } from './types/timer';
+//import type { Timer } from './types/timer';
 import './App.css'
 
-  const initialTimers: Timer[] = [
+function App() {
+  
+  const [timers, setTimers] = useState([
     {
       id: crypto.randomUUID(),
       name: 'training1',
       randomMin: 1,
-      randomMan: 3,
+      randomMax: 3,
       trainingTime: 4,
       interval: 5,
       rounds: 6,
@@ -17,20 +19,30 @@ import './App.css'
       id: crypto.randomUUID(),
       name: 'training2',
       randomMin: 1,
-      randomMan: 3,
+      randomMax: 3,
       trainingTime: 4,
       interval: 5,
       rounds: 6,
     }
-  ];
 
-function App() {
-  
+  ]);
 
   return (
     <main className="timer-app">
       <h1>ReactionTimer</h1>
       <p>好機を捉える初動を磨く</p>
+
+      <ul>
+        {timers.map((timer) => (
+          <li key={timer.id}>
+            {timer.name}
+            {timer.randomMin}
+            {timer.randomMax}
+            
+            
+          </li>
+        ))}
+      </ul>
       <TimerForm />
 
     </main>
