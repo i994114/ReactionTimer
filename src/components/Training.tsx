c2643351df91e43d5860c57ef0beb4a3bb71a036
i994114 <i994114@hotmail.com>
2026-09-09 13:45:22 +0900

    開始のダウンタイマーを実装

--- src/components/Training.tsx
+++ src/components/Training.tsx
@@ -1,6 +1,25 @@
+import { useState, useEffect, use } from "react";
+import { useParams } from "react-router-dom";
+
 function Training() {
+  const {id} = useParams();
+  const [countDown, setCountDown] = useState(3);
+
+  /* 開始ダウンタイマー */
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCountDown((current) => current > 0? current - 1 : current);
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  },[]);
+  
+  
   return (
-    <h1>Training!!!!</h1>
+    <div>
+      <h1>Training!!!!</h1>
+      <h2>{countDown}</h2>
+    </div>
   );
 }
 
