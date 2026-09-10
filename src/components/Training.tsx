55530bbc84369e33c02d36a3d713b201d874f098
i994114 <i994114@hotmail.com>
2026-09-10 10:33:46 +0900

    各タイマーが設定値のとおり動くように実装

--- src/components/Training.tsx
+++ src/components/Training.tsx
@@ -4,15 +4,23 @@ import { loadTimer } from "../utils/timerStorage";
 import { useNavigate } from "react-router-dom";
 
 function Training() {
+  type Phase = 'countdown' | 'training' | 'interval' | 'finished';
+  const [phase, setPhase] = useState<Phase>('countdown');
+
   const navigate = useNavigate();
   const {id} = useParams();
   const timer =  id? loadTimer(id) : undefined;
   const [startCountDown, setStartCountDown] = useState(3);
   const [trainingTime, setTrainingTime] = useState(timer?.trainingTime);
   const [intervalTime, setIntervalTime] = useState(timer?.interval);
+  const [currentRound, setCurrentRound] = useState(1);
 
   //開始ダウンタイマー
   useEffect(() => {
+    if (phase !== 'countdown') {
+      return;
+    }
+
     const timerId = setInterval(() => {
       setStartCountDown((current) => {
         if (current > 1) {
@@ -22,14 +30,24 @@ function Training() {
         return 0;
       });
     }, 1000);
-
     return () => clearInterval(timerId);
-  },[]);
+  },[phase]);
   
+  //開始カウントダウン終了後
+  useEffect(() => {
+    if (phase !== 'countdown') {
+      return;
+    }
+    if (startCountDown !== 0) {
+      return;
+    }
+
+    setPhase('training');
+  }, [phase, startCountDown]);
+
   //トレーニング時間
   useEffect(() => {
-    //開始タイマー作動中はカウントしない
-    if (startCountDown > 0) {
+    if (phase !== 'training') {
       return;
     }
   
@@ -39,37 +57,74 @@ function Training() {
           return current - 1;
         }
         clearInterval(timerId);
+
         return 0;
       });
     },1000)
     return () => clearInterval(timerId);
-  },[startCountDown]);
+  },[phase]);
   
+  //トレーニング終了後
+  useEffect(() => {
+    if (phase !== 'training') {
+      return;
+    }
+    if (trainingTime !== 0) {
+      return;
+    }
+
+    if (currentRound < timer.rounds) {
+      setPhase('interval');
+    } else {
+      setPhase('finished');
+    }
+  }, [phase, trainingTime]);
+
   //インターバル時間
   useEffect(() => {
-    //トレーニング時間中はカウントしない
-    if (trainingTime > 0) {
+    
+    if (phase !== 'interval') {
       return;
     }
 
     const timerId = setInterval(() => {
       setIntervalTime((current) => {
-        if (current > 1) {
+        if (current !== undefined && current > 0) {
           return current - 1;
         }
-        clearInterval(timerId);
+
         return 0;
       });
     }, 1000);
-    return () => clearInterval(timerId);
-  }, [trainingTime]);
 
+    return () => clearInterval(timerId);
+  }, [phase]);
+
+  //インターバル終了後
+  useEffect(() => {
+    if (phase !== 'interval') {
+      return;
+    }
+
+    if (intervalTime !== 0) {
+      return;
+    }
+
+    if (!timer) {
+      return;
+    }
+
+    setCurrentRound((current) => current + 1);
+    setTrainingTime(timer.trainingTime);
+    setIntervalTime(timer.interval);
+    setPhase('training');
+  }, [intervalTime, phase, timer])
 
   return (
     <div>
       <h1>{timer?.name}</h1>
       <button onClick={() => navigate('/')} >一覧に戻る</button>
-      {startCountDown > 0? (
+      {phase === 'countdown' && (
         <>
           <h2>トレーニング開始まで</h2>
           <h2>{startCountDown}</h2>
@@ -78,18 +133,23 @@ function Training() {
           <p>インターバル：{timer?.interval}</p>
           <p>ターン数：{timer?.rounds}</p>        
         </>
-      ) : (
+      )}
+      
+      {phase === 'training' && (
         <>
+          <h2>{currentRound} / {timer?.rounds}</h2>
           <h1>トレーニング中</h1>
           <h2>{formatTime(trainingTime)}</h2>
           <h2>/{formatTime(timer.trainingTime)}</h2>
+        </>
+      )}
 
+      {phase === 'interval' && (
+        <>
           <h1>インターバル中</h1>
           <h2>{formatTime(intervalTime)}</h2>
           <h2>/{formatTime(timer?.interval)}</h2>
-
         </>
-        
       )}
       
     </div>
