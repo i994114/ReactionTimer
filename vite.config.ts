42c6ab79dfd21c7c228d0a698fa07b5c96b9cc46
i994114 <i994114@hotmail.com>
2026-09-13 15:01:50 +0900

    PWA用ファイル設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '反応トレーニング',
+        short_name: '反応トレ',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#F8FAFC',
+        theme_color: '#2563EB',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
