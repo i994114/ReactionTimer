9765540d069c931a918769dacae8c0b55d7bdda9
i994114 <i994114@hotmail.com>
2026-09-13 15:32:05 +0900

    distフォルダのパスがデプロイ用URLとなっていなかったので修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,30 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/ReactionTimer/',
+
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+
       manifest: {
         name: '反応トレーニング',
         short_name: '反応トレ',
-        start_url: '/',
+        start_url: '/ReactionTimer/',
+        scope: '/ReactionTimer/',
         display: 'standalone',
         background_color: '#F8FAFC',
         theme_color: '#2563EB',
+
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/ReactionTimer/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: '/ReactionTimer/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
